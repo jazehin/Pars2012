c41542527f7ea5d60d0424d26d98c9c3c8ef31ba
kiss.bence2 <kiss.bence2@28-05>
2023-01-16 11:23:50 +0100

    Add project files.

---
+++ Pars2012/Program.cs
@@ -0,0 +1,70 @@
+﻿using Pars2012.Properties;
+
+namespace Pars2012
+{
+    internal class Program
+    {
+        static List<Versenyzo> versenyzok = new();
+        static string eleresiUt = @"..\..\..\Resources\";
+        static void Main()
+        {
+            F04();
+            F05();
+            F06();
+            F09();
+            F10();
+            Console.ReadKey();
+        }
+
+        private static void F10()
+        {
+            using StreamWriter sw = new($"{eleresiUt}Dontos2012.txt");
+
+            List<Versenyzo> dontobeJutottak = versenyzok.OrderByDescending(versenyzo => versenyzo.Eredmeny).Take(12).ToList();
+
+            sw.WriteLine("Helyezés;Név;Csoport;Nemzet;NemzetKód;Sorozat;Eredmény");
+            for (int i = 0; i < dontobeJutottak.Count; i++)
+            {
+                sw.WriteLine($"{i + 1};{dontobeJutottak[i].ToString()}");
+            }
+            sw.Flush();
+            sw.Close();
+        }
+
+        private static void F09()
+        {
+            Versenyzo legjobb = versenyzok.OrderByDescending(versenyzo => versenyzo.Eredmeny).First();
+
+            Console.WriteLine("9. feladat: A selejtező nyertese:");
+            Console.WriteLine($"\tNév: {legjobb.Nev}");
+            Console.WriteLine($"\tCsoport: {legjobb.Csoport}");
+            Console.WriteLine($"\tNemzet: {legjobb.Nemzet}");
+            Console.WriteLine($"\tNemzet kód: {legjobb.Kod}");
+            Console.WriteLine($"\tSorozat: {legjobb.DobasokAsString}");
+            Console.WriteLine($"\tEredmény: {legjobb.Eredmeny}");
+        }
+
+        private static void F06()
+        {
+            int tovabbjutottakNagyDobassal = versenyzok.Count(versenyzo => versenyzo.Dobasok[1] == -2f || versenyzo.Dobasok[2] == -2f);
+
+            Console.WriteLine($"6.feladat: 78,00 méter feletti eredménnyel továbbjutott: {tovabbjutottakNagyDobassal} fő");
+        }
+
+        private static void F05()
+        {
+            Console.WriteLine($"5. feladat: Versenyzők száma a selejtezőben: {versenyzok.Count} fő");
+        }
+
+        private static void F04()
+        {
+            using StreamReader sr = new($"{eleresiUt}Selejtezo2012.txt");
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                versenyzok.Add(new(sr.ReadLine()));
+            }
+        }
+    }
+}
---
+++ Pars2012/Versenyzo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pars2012
+{
+    internal class Versenyzo
+    {
+        readonly string _nev;
+        readonly char _csoport;
+        readonly string _nemzetEsKod;
+        readonly double[] _dobasok = new double[3];
+
+        public Versenyzo(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            _nev = adatok[0];
+            _csoport = adatok[1][0];
+            _nemzetEsKod = adatok[2];
+
+            for (int i = 3; i < adatok.Length; i++)
+            {
+                switch (adatok[i])
+                {
+                    case "X":
+                        _dobasok[i - 3] = -1f;
+                        break;
+                    case "-":
+                        _dobasok[i - 3] = -2f;
+                        break;
+                    default:
+                        _dobasok[i - 3] = double.Parse(adatok[i]);
+                        break;
+                }
+            }
+        }
+
+        public string Nev { get => _nev; }
+        public char Csoport { get => _csoport;  }
+        public string Nemzet { get => _nemzetEsKod.Substring(0, _nemzetEsKod.Length - 6); }
+        public string Kod { get => _nemzetEsKod.Substring(_nemzetEsKod.Length - 4, 3); }
+        public double[] Dobasok { get => _dobasok; }
+        public string DobasokAsString
+        {
+            get
+            {
+                string[] dobasok = new string[_dobasok.Length];
+                for (int i = 0; i < dobasok.Length; i++)
+                {
+                    switch (Dobasok[i])
+                    {
+                        case -2:
+                            dobasok[i] = "-";
+                            break;
+                        case -1:
+                            dobasok[i] = "X";
+                            break;
+                        default:
+                            dobasok[i] = $"{Dobasok[i]}";
+                            break;
+                    }
+                }
+                return $"{dobasok[0]};{dobasok[1]};{dobasok[2]}";
+            }
+        }
+        public double Eredmeny { get =>Dobasok.Max(); }
+
+        public override string ToString()
+        {
+            return $"{Nev};{Csoport};{Nemzet};{Kod};{DobasokAsString};{Eredmeny}";
+        }
+
+    }
+}
